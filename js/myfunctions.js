243aefba943526123179ddb168886bfd7d50a00c
ESTHER WAHNON AMSELEM <estherwa@hotmail.es>
2021-11-10 14:40:37 +0200

    new changes

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,84 +1,36 @@
 
 
-    class Task
-    {
-        constructor(title, description, highPriority){
-            this.title = title;
-            this.description = description;
-            this.highPriority = highPriority;
-        }
-    }
-
-    let tasksList = []
-
-    class Course {
-    #example = ""; // example of private member
-
-    // the ctor
-    constructor (courseName, credits, lecturer) {
-    this.courseName = courseName;
-    this.credits = credits;
-    this.lecturer = lecturer;
-}
-    #examplePrivate() { // example of private method
-}
-}
-
-    // an array of courses and a string of the HTML to display
-    let coursesList = [];
-    let result = "";
-    const separator = ", "
-
-    function printCompact (course) {
-    return course.courseName;
-}
-
-    function printFullDetails (course) {
-    return (course.courseName
-    + separator + course.lecturer
-    + separator + course.credits
-    + " credits ");
-}
-
-    // function expression, arrow style
-    let addCourse =  (course) => {
-    coursesList.push(course);
-}
-    // same as
-    // function addCourse(course) {
-    //     coursesList.push(course);
-    // }
-
-    //function buildHTMLCourseList (printFunct) {
-    var buildHTMLCourseList =  (printFunct) => {
-    var result = "<h2>List of Courses</h2><ol>";
-    for (var c of coursesList) {
-    result += "<li>" + printFunct(c) + "</li>";
-}
-    result += "</ol>";
-    return result;
-}
-
-
-    // TESTING OUR CODE  - in general there is no such code in global scope
-    // initialize the array of courses
-    addCourse(new Course("Internet Programming", 4, "Dr. Cohen"));
-    addCourse(new Course("Architecture", 5, "Dr. Levy"));
-
-    // PREPARE THE BUTTONS LISTENERS for testing
-    // wait for the DOM before reaching elements
-    window.addEventListener('DOMContentLoaded', (event) => {
-
-    document.getElementById("compact").addEventListener('click', () => {
-    // display the HTML
-    document.getElementById("clist").innerHTML = buildHTMLCourseList(printCompact);
+document.addEventListener('DOMContentLoaded', () =>
+{
+    document.querySelector("button ").addEventListener("click", createItem);
 });
 
-    document.getElementById("details").addEventListener('click', () => {
-    // display the HTML
-    document.getElementById("clist").innerHTML = buildHTMLCourseList(printFullDetails);
-});
-});
+function deleteItem(event)
+{
+
+    this.parentElement.remove();
+}
+
+function createItem()
+{
+    let list = document.getElementById("list1") ;
+
+    // create and insert the <li> item using DOM functions (method 1)
+    let newitem = document.createElement("li");
+    // read input text and create the text node
+    let newtext = document.createTextNode(document.getElementById("user-input").value);
+    newitem.appendChild(newtext);
+
+    // add the X button next to the new item
+    let b = document.createElement("button");
+    b.innerHTML = "<span> X </span>"; //insert  manually HTML string (method 2)
+
+    newitem.appendChild(b);
+
+    b.addEventListener('click', deleteItem); // removeListener
+
+    list.appendChild(newitem); // attach the new item
+}
 
 
 
